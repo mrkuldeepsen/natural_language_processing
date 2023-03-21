9400ceb24756099a30dfee3002f322bc29c67e87
harshitigtechso100 <harshit.igtechso@gmail.com>
2023-03-21 17:38:40 +0530

    faceing issue on get audio from user

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const express = require('express') 
+const express = require('express')
+const app = express() 
 const path = require('path');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 require("dotenv").config();
 const {Leopard} = require("@picovoice/leopard-node");
 const { NlpManager } = require('node-nlp');
-const {manager} = require('./train-nlp');
 const fs = require('fs') ;
 const { LangEn } = require('@nlpjs/lang-en');
 const { ConsoleConnector } = require('@nlpjs/console-connector');
 const trainnlp = require('./train-nlp')
-const { SpeechRecorder } = require("speech-recorder")
-const { devices } = require("speech-recorder")
-const Recorder = require('audio-recorder-js');
+var ks = require('node-key-sender')
 
 
-
-
-
-const app = express()
 const PORT  = process.env.PORT || 5001 ;
 
 //middlewares
@@ -31,30 +28,39 @@ const accessKey = process.env.Access_key;
 const handle = new Leopard(accessKey);
 const ans = [] ;
 
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+let value = ''
 app.get('/', async (req,res) => {
+    value = req.body.p;
+    //console.log(value)
     res.status(200).render('home',{
         values:ans,
     });
-
 })
+
 // Train and save the model.
 const nlp = new NlpManager({ languages: ['en'], threshold: 0.5 });
 nlp.container.register('fs', fs);
 //app.use(LangEn);
 
 const connector = new ConsoleConnector();
-connector.onHear = async (parent, line) => {
-  
-         result = await nlp.process(line);
+
+connector.onHear = async (line ) => {
+        line = value ;
+        result = await  nlp.process(line);
+        ks.sendKey('enter');
         if(result.answer){
             ans.push(result.answer);
             connector.say(result.answer);
-            
+
         }else{
-            ans.push(result.answer);
+            ans.push('i am not capable');
             connector.say('i am not capable');
         }
-  
 };
 
 (async () => {
@@ -62,7 +68,6 @@ connector.onHear = async (parent, line) => {
     connector.say('Ask me');
 })();
 
-
-app.listen(PORT,()=>{
+http.listen(PORT,()=>{
     console.log(`app is working on port: ${PORT}`)
 })
